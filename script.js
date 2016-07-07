7e3787a6ae56dc12c831ac886779026b5e485251
Sheng0o0 <shenghaozheng@mybihs.org>
2016-07-07 18:33:02 +0000

    foursquare

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"https://api.foursquare.com/v2/venues/search
+  ?client_id=TANBIGMKJ5H3TNHMZXDCNU4RTDJR3HZZ3BP2XK20T3PRRBI1
+  &client_secret=R3XNQ1TCZBU4TDF011W4ST3QFRJDMINWUJ0UW55NHILYZX3X
+  &v=20130815
+  &ll=40.7,-74
+  &query=sushi",
+  success: function(result){
+      console.log(result);
+  }
+    )}:
+});
